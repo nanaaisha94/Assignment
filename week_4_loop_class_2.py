54035dc3a7a3492897996c871679fc8a0aafc4ec
unknown <abubakar.nana94@gmail.com>
2024-06-24 23:32:17 +0100

    week_4_loop_class_2 is the assignment
    git status

---
+++ week_4_loop_class_2.py
@@ -0,0 +1,15 @@
+LG_in_Kwara = ['ilorin west','ekiti', 'oke ero','ifelodun','barutee','asa','ilorin south','kiama'] 
+# # length = len(LG_in_Kwara)
+# # print(length)
+# for L in LG_in_Kwara:
+#     print(len(L))
+# Length=len(LG_in_Kwara)
+# print(Length)
+# for i in range(0,Length,2):
+#     print(LG_in_Kwara[i])
+for l in LG_in_Kwara:
+    print(l+' LGA')
+
+
+
+
