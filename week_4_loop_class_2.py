f6943a2ed5c78ef01685e1c1da673e8d8c3c885a
unknown <abubakar.nana94@gmail.com>
2024-06-25 22:42:39 +0100

    loop assignment

--- week_4_loop_class_2.py
+++ week_4_loop_class_2.py
@@ -1,14 +1,20 @@
 LG_in_Kwara = ['ilorin west','ekiti', 'oke ero','ifelodun','barutee','asa','ilorin south','kiama'] 
-# # length = len(LG_in_Kwara)
-# # print(length)
+# length = len(LG_in_Kwara)
+# print(length)
 # for L in LG_in_Kwara:
 #     print(len(L))
 # Length=len(LG_in_Kwara)
 # print(Length)
 # for i in range(0,Length,2):
 #     print(LG_in_Kwara[i])
-for l in LG_in_Kwara:
-    print(l+' LGA')
+LGA=[L + ' lga' for L in LG_in_Kwara]
+print(LGA)
+
+
+
+
+
+    
 
 
 
